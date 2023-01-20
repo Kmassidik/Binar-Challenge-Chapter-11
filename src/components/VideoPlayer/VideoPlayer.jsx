8b41c43ffdbebc2d126bb473e870af374f0de10e
NathanzaGaurangga <nathnzagaurangga@gmail.com>
2023-01-20 21:35:46 +0800

    test videoplayer

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,56 @@
+export default function VideoPlayer() { 
+    const media_urls = [
+      {
+        id: 1,
+        title: 'Video One',
+        video_url:
+          'https://res.cloudinary.com/dlxw2mnrp/video/upload/v1674188873/random/testvideo_nnhbot.mp4',
+      },
+    ]
+  
+    // handle mouse enter
+    const handleMouseEnter = (e) => {
+      const vid = e.target
+    //   vid.muted = true
+      vid.play()
+    }
+  
+    // handle mouse leave
+    const handleMouseLeave = (e) => {
+      const vid = e.target
+    //   vid.muted = false
+      vid.currentTime = 0
+      vid.pause()
+    }
+  
+    return (
+      <>
+        <h1 className='text-center'>Cloudinary Video Show</h1>
+        <div className='container'>
+          <div className='row'>
+            {media_urls.map((media) => (
+              <div key={media.id} className='col-lg-4 col-sm-6 mb-4'>
+                <div className='card h-100'>
+                  <div className='card-body'>
+                    <h4 className='card-title'>{media.title}</h4>
+                    <video
+                      width='320'
+                      height='240'
+                      controls
+                      onMouseEnter={handleMouseEnter}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      <source src={media.video_url} type='video/mp4' />
+                    </video>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    )
+  }
+  
+
+  
