6c7c9361994221e04d0f62e773192fcc31e4e110
Fajar Putra H <fajarph.dev@gmail.com>
2023-01-21 17:57:54 +0700

    add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
-import Login from "./pages/Login";
-
+import { Footer } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Footer/>
     </div>
   );
 }
