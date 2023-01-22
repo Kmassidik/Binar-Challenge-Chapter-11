db22dac742cb085770f69899572b432a02ad711c
Kurnia Massidik <dev@Kurnias-MacBook-Pro.local>
2023-01-22 10:01:23 +0700

    test deploy after problem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import { ModalLogin } from "./components";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <ModalLogin></ModalLogin>
     </div>
   );
 }
