0e522fcc608ad7c827d66089c95889a783a3ec84
Nur Fajri Qas <nfq.fajri28@gmail.com>
2023-01-22 12:18:22 +0700

    pdfbutton

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import ProfileComponent from "../components/profile/ProfileComponent"
+import Button from "../components/button/ButtonDownload"
 
 export default function Profile() {
     return(
         <div className="vh-100">
             <ProfileComponent />
+            <Button/>
         </div>
     )
 }
